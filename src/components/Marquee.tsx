1fd20e8931b716aea7990209d01b0e3ed1cfbcf9
fgirse <fgirs@bluewin.ch>
2026-02-15 00:45:27 +0100

    modify hero and productOfTheMonth

--- src/components/Marquee.tsx
+++ src/components/Marquee.tsx
@@ -1,21 +1,53 @@
 import React from "react";
-import AstraLogo from "../components/AstraLogo";
-import FlensLogo from "../components/FlensLogo";
-import LogoNeu from "../components/LogoNeu";
+import AstraLogo from "../../public/Assets/Svg/AstraLogo.svg";
+import FlensLogo from "../../public/Assets/Svg/LogoFlens.svg";
+import LogoNeu from "../../public/Assets/Svg/faviconLogoNeu.svg"; 
+import Image from "next/image";
 import Marquee from "react-fast-marquee";
 
 const App = () => (
   <Marquee>
-    <div className="flex items-center gap-40">
-    <AstraLogo />
-    <FlensLogo />
-    <LogoNeu />
-    <AstraLogo />
-    <FlensLogo />
-    <LogoNeu />
-    <AstraLogo />
-    <FlensLogo />
-    <LogoNeu />
+    <div className="flex items-center gap-32">
+
+    
+  
+    <Image
+    src={LogoNeu}
+    alt="LogoNeu"
+    width={144}
+    height={32}
+    /> 
+    <Image
+    src={AstraLogo}
+    alt="LogoNeu"
+    width={144}
+    height={32}
+    /> 
+    <Image
+    src={FlensLogo}
+    alt="LogoNeu"
+    width={144}
+    height={32}
+    /> 
+    <Image
+    src={LogoNeu}
+    alt="LogoNeu"
+    width={144}
+    height={32}
+    /> 
+    <Image
+    src={AstraLogo}
+    alt="LogoNeu"
+    width={144}
+    height={32}
+    /> 
+    <Image
+    src={FlensLogo}
+    alt="LogoNeu"
+    width={144}
+    height={32}
+    /> 
+   
     </div>
   </Marquee>
 );
--- src/components/productOfTheMonth.tsx
+++ src/components/productOfTheMonth.tsx
@@ -28,11 +28,11 @@ export default async function ProductOfTheMonth() {
   return (
     <>
       {/* Product of the Month Section */}
-      <div className="absolute top-[60vh] md:top-[70vh] lg:top-48 lg:left-12 -rotate-20 max-w-2xl lg:mt-12 lg:w-full lg:max-w-4xl z-10">
+      <div className="absolute top-[60vh] md:top-[60vh] lg:top-48 lg:left-12 -rotate-20 max-w-2xl lg:mt-12 lg:w-full lg:max-w-4xl z-10">
         <div className="relative md:overflow-hidden rounded-2xl bg-linear-to-br from-lime-300/10 to-lime-500/70 lg:from-lime-300 lg:to-lime-500/70 lg:bg-linear-to-b backdrop-blur-sm border border-amber-500/20 p-8 shadow-2xl">
           <div className="grid md:grid-cols-2 gap-8 items-center">
             {/* Image Section */}
-            <div className="relative h-32 w-[60vw] md:h-80 lg:w-[10vw] rounded-xl overflow-hidden group">
+            <div className="relative h-32 w-[30vw] md:h-80 md:w-[20vw] lg:w-[20vw] rounded-xl overflow-hidden group">
               <Image
                 src={productImage}
                 alt={productOfTheMonth.title || 'Product of the Month'}
@@ -52,7 +52,7 @@ export default async function ProductOfTheMonth() {
             <div className="space-y-4 text-left">
               <div className="space-y-2">
                 {productOfTheMonth.subtitle && (
-                  <p className=" text-yellow-600 text-[5vw] leading-10 headingA font-bold uppercase lg:text-[2vw] lg:leading-12  tracking-wider">
+                  <p className=" text-yellow-600 text-[5vw] leading-10 headingA md:text-[4vw] font-bold uppercase lg:text-[2vw] lg:leading-12  tracking-wider">
                     {productOfTheMonth.subtitle}
                   </p>
                 )}
